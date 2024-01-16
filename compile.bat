e732fbe91e0a2e9a45645bccacb423ae217d231f
MADRAFi <madrafi@gmail.com>
2024-01-16 01:10:18 +0100

    path update

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @Echo off
 echo Comiling
-c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mp.exe shon.pas -define:DEBUG
-if exist shon.a65 c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mads.exe shon.a65 -x -i:c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\base -o:shon_uncmp.xex
+mp.exe shon.pas -define:DEBUG
+if exist shon.a65 mads.exe shon.a65 -x -i:D:\Dropbox\Atari\DEV\MADS\base -o:shon_uncmp.xex
 if not %ERRORLEVEL%==0 pause
 echo.
 
